ccbe2f7a6774227d3ce39439aad7029afd58b2d2
Egor B <egor.v.boitsov@gmail.com>
2022-03-27 01:58:45 +0300

    rows-of-antidiagonals, but with outer loop outside the kernel

--- src/fpga_main.hpp
+++ src/fpga_main.hpp
@@ -303,6 +303,7 @@ inline void semi_process_cell(int A, int B, int &H, int &V)
 	if (need_swap) semi_swap(H, V);
 }
 
+#if 0
 void semi_simple(LcsProblem &p)
 {
 	auto &q = GetFpgaQueue();
@@ -347,7 +348,7 @@ void semi_simple(LcsProblem &p)
 							// load everything from the left
 							int Hs[ROW_M];
 							int As[ROW_M];
-							
+
 							#pragma unroll
 							for (int ii = 0; ii < ROW_M; ++ii)
 							{
@@ -393,3 +394,143 @@ void semi_simple(LcsProblem &p)
 	p.perm_hash = result.hash();
 }
 
+#endif
+
+// antidiagonals-in-rows
+void semi_simple(LcsProblem &p)
+{
+	auto &q = GetFpgaQueue();
+
+	int m = p.size_a;
+	int n = p.size_b;
+
+	int *a_data = new int[m];
+	int *b_data = new int[n];
+
+	int *h_strands_data = new int[m];
+	int *v_strands_data = new int[n];
+
+	for (int i = 0; i < m; ++i) a_data[i] = p.input_a[m - i - 1];
+	for (int j = 0; j < n; ++j) b_data[j] = p.input_b[j];
+
+	for (int i = 0; i < m; ++i) h_strands_data[i] = i;
+	for (int j = 0; j < n; ++j) v_strands_data[j] = m + j;
+
+	constexpr int ROW_M = GLOBAL_WINDOW_SIZE;
+
+
+	int row_count = SmallestMultipleToFit(ROW_M, m);
+
+	{
+		sycl::buffer<int, 1> buf_a(a_data, m);
+		sycl::buffer<int, 1> buf_b(b_data, n);
+		sycl::buffer<int, 1> buf_h_strands(h_strands_data, m);
+		sycl::buffer<int, 1> buf_v_strands(v_strands_data, n);
+
+		for (int row = 0; row < row_count; ++row)
+		{
+			int i_bottom = m - (row + 1) * ROW_M;
+			
+			q.submit([&](auto &h)
+				{
+					auto a = buf_a.get_access<sycl::access::mode::read>(h);
+					auto b = buf_b.get_access<sycl::access::mode::read>(h);
+					auto h_strands = buf_h_strands.get_access<sycl::access::mode::read_write>(h);
+					auto v_strands = buf_v_strands.get_access<sycl::access::mode::read_write>(h);
+
+					h.single_task([=]()
+						{
+							
+							// local registers
+							int Hs[ROW_M];
+							int As[ROW_M];
+
+							// shift register
+							int Vs[ROW_M];
+							int Bs[ROW_M];
+
+
+							#pragma unroll
+							[[intel::ivdep]]
+							for (int ii = 0; ii < ROW_M; ++ii)
+							{
+								Hs[ii] = h_strands[i_bottom + ii];
+							}
+
+							#pragma unroll
+							[[intel::ivdep]]
+							for (int ii = 0; ii < ROW_M; ++ii)
+							{
+								As[ii] = a[i_bottom + ii];
+							}
+
+							[[intel::ivdep]]
+							for (int horz_step = 1 - ROW_M; horz_step < n; ++horz_step)
+							{
+								int j_left = horz_step;
+								int j_right = horz_step + ROW_M - 1;
+
+								if (j_right < n)
+								{
+									Vs[ROW_M - 1] = v_strands[j_right];
+									Bs[ROW_M - 1] = b[j_right];
+								}
+
+								#pragma unroll
+								[[intel::ivdep]]
+								for (int step = 0; step < ROW_M; ++step)
+								{
+									int i = i_bottom + step;
+									int j = j_left + step;
+
+									int ii = step;
+									int jj = step;
+
+									bool inside = 0 <= j && j < n && i >= 0;
+
+									int h = Hs[ii];
+									int v = Vs[jj];
+									bool need_swap = (As[ii] == Bs[jj] || h > v) && inside;
+
+									Hs[ii] = need_swap ? v : h;
+									Vs[jj] = need_swap ? h : v;
+								}
+
+								// store V
+								if (j_left >= 0)
+								{
+									v_strands[j_left] = Vs[0];
+								}
+
+								// shift register
+								#pragma unroll
+								for (int jj = 0; jj < ROW_M - 1; ++jj)
+								{
+									Vs[jj] = Vs[jj + 1];
+									Bs[jj] = Bs[jj + 1];
+								}
+							}
+
+							// store h_strands back
+							#pragma unroll
+							for (int ii = 0; ii < ROW_M; ++ii)
+							{
+								h_strands[i_bottom + ii] = Hs[ii];
+							}
+
+						}
+					);
+				}
+			);
+		}
+	}
+
+	PermutationMatrix result = PermutationMatrix::FromStrands(h_strands_data, m, v_strands_data, n);
+
+	delete[] a_data;
+	delete[] b_data;
+	delete[] h_strands_data;
+	delete[] v_strands_data;
+
+	p.perm_hash = result.hash();
+}
